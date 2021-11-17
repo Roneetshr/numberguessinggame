fae61e40c90badce7ee6dbbd9e7f132c9ddb643c
Roneetshr <73659639+Roneetshr@users.noreply.github.com>
2021-11-17 21:41:42 +0000

    Add files via upload

---
+++ numberguessing.py
@@ -0,0 +1,38 @@
+number1 = 1
+number2 = 2
+number3 = 3
+number4 = 4
+number5 = 5
+number6 = 6
+number7 = 7
+number8 = 8
+number9 = 9
+numberguess = input("guess a number: ")
+print numberguess
+
+if numberguess = 9
+numberis = number1
+
+if numberguess = 8
+numberis = number2
+
+if numberguess = 7
+numberis = number3
+
+if numberguess = 6
+numberis = number4
+
+if numberguess = 5
+numberis = number5
+
+if numberguess = 4
+numberis = number6
+
+if numberguess = 3
+numberis = number7
+
+if numberguess = 2
+numberis = number8
+
+if numberguess = 1
+numberis = number9
